d55d70a787d73c87a1fd678625c6616484f76e48
Sifan Momin <114792773+sifanmomin@users.noreply.github.com>
2025-02-14 11:17:24 +0530

    Add files via upload

---
+++ my-folder/model-folder/langugaemodel-folder/llmgroq.py
@@ -0,0 +1,18 @@
+from langchain_groq import ChatGroq
+from dotenv import load_dotenv
+import os
+
+# Load environment variables
+load_dotenv()
+
+# Hardcoded API Key (Use with caution)
+groq_api_key = "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"
+
+# Initialize Groq LLM (Chat-based model)
+llm = ChatGroq(model_name="mixtral-8x7b-32768", groq_api_key=groq_api_key,temperature= 0.3,max_tokens=50)
+
+# Invoke the model
+result = llm.invoke("write the 5 line poem on cricket?")
+
+# Print the result
+print(result.content)
